3cb6bb0de7f49c765ce589f04df78e4145ac1ce1
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2024-01-18 12:10:04 -0500

    fix: set opacity after doodle element is mounted

--- src/components/Doodle.tsx
+++ src/components/Doodle.tsx
@@ -5,13 +5,30 @@ function Doodle({ children, ...props }: React.HTMLProps<HTMLDivElement>) {
   useScript({
     url: "https://cdnjs.cloudflare.com/ajax/libs/css-doodle/0.38.3/css-doodle.min.js",
   });
-  const [opacity, setOpacity] = useState(0)
+  const [opacity, setOpacity] = useState(0);
 
   const el = React.useMemo(
-    () => React.createElement("css-doodle", {...props, style: {...props.style, opacity, transition: 'opacity 0s ease-in-out 0s' }}, `${children}`, children),
+    () =>
+      React.createElement(
+        "css-doodle",
+        {
+          ...props,
+          style: {
+            ...props.style,
+            opacity,
+            transition: "opacity 0s ease-in-out 0s",
+          },
+        },
+        `${children}`,
+        children
+      ),
     [children, opacity, props]
   );
-  setTimeout(() => setOpacity(1))
+
+  React.useEffect(() => {
+    setTimeout(() => setOpacity(1));
+  }, []);
+
   return el;
 }
 
